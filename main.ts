6451af6ad637454008360d3705a7a1531d4e3c20
pnrgrr0505 <127877261+pnrgrr0505@users.noreply.github.com>
2023-03-30 22:12:02 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (0 == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    radio.sendNumber(1)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(0)
+})
+basic.forever(function () {
+	
+})
